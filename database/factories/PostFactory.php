b521a0ca33f52b468d677c54d2aa3ef8d7608f84
FLU <valerio.massj@gmail.com>
2022-02-04 19:47:35 +0100

    update: created entity with faker posts

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker -> sentence(3, true),
+        'author' => $faker -> name(),
+        'subtitle' => $faker -> words(3, true),
+        'content' => $faker -> text(200),
+        'publish_date' => $faker -> date(),
+        'likes' => $faker -> randomNumber(5, false),
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+/* 
+Oltre alle rotte di autenticazione che già conoscete (/login, /logout, /register) avrete:
+- ok - una pagina in cui mostrare i form di autenticazione (come avete già visto in questi giorni)
+- una  rotta /posts in cui mostrare tutti i post presenti nel db
+- una rotta /posts/create in cui è presente il form per creare un nuovo post
+- una rotta /posts/store da chiamare dal form per creare effettivamente l'entità (ma questo lo sapete già)
+*/
 
 /* Auth::routes(); */
 
