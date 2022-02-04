3c223e97224d9a25f69f5dd044fcb15b54698de0
FLU <valerio.massj@gmail.com>
2022-02-04 18:37:09 +0100

    update: login, logout and register done

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,49 @@
+<header>
+    <h2>Header</h2>
+    @auth
+        <h2>Welcome {{Auth::user() -> name}}</h2>
+        <a href="{{route('logout')}}" class="btn btn-secondary">Logout</a>
+    
+        @else
+        <h2>Login or Register</h2>
+        
+    @endauth
+    @guest
+        <nav>
+            <h2>Login</h2>
+
+            <form action="{{route('login')}}" method="POST">
+
+                @method('POST')
+                @csrf
+
+                <label for="email">Email</label><br>
+                <input type="email" name="email"><br>
+                <label for="password">Password</label><br>
+                <input type="password" name="password"><br>
+
+                <input type="submit" value="Login">
+            </form>
+        </nav>
+        <nav>
+            <h2>Register</h2>
+
+            <form action="{{route('register')}}" method="POST">
+
+                @method('POST')
+                @csrf
+                
+                <label for="name">Name</label><br>
+                <input type="text" name="name"><br>
+                <label for="email">Email</label><br>
+                <input type="email" name="email"><br>
+                <label for="password">Password</label><br>
+                <input type="password" name="password"><br>
+                <label for="password_confirmation">Password Confirmation</label><br>
+                <input type="password" name="password_confirmation"><br>
+
+                <input type="submit" value="Register">
+            </form>
+        </nav>
+    @endguest
+</header>
